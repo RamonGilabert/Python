0d4d04ac53ce25bcb5998131ebd510976461937a
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-04-12 20:36:30 +0200

    Adds the basic skeleton of the python app

--- Second project/app.py
+++ Second project/app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def main_view():
+    return render_template("index.html")
+
+@app.route("/insert_user", methods=['POST', 'GET'])
+def insert_view():
+    return render_template("insert.html")
+
+@app.route("/show_users", methods=['GET'])
+def show_view():
+    return render_template("show.html")
+
+@app.route("/login", methods=['POST', 'GET'])
+def login_view():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run()
