7a3bccbf085456e8557bdd751b07a31a4a454fd3
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-04-14 19:28:01 +0200

    Adds the database just if it doesn't exist

--- Second project/connection.py
+++ Second project/connection.py
@@ -3,7 +3,7 @@ from flask import g
 from contextlib import closing
 
 class Connection:
-    
+
     def __init__(self, app):
         self.app = app
 
--- Second project/schema.sql
+++ Second project/schema.sql
@@ -1,10 +1,7 @@
-DROP TABLE IF EXISTS Users;
-CREATE TABLE Users (
+CREATE TABLE IF NOT EXISTS Users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT NOT NULL UNIQUE,
   name TEXT NOT NULL,
   email TEXT NOT NULL,
   password TEXT NOT NULL
 );
-
-INSERT INTO Users (username, name, email, password) VALUES ("Ramon", "Ramon", "ramon@hyper.no", "123")
