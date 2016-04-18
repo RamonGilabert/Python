2470375714a04d77a1179a10ab595b577eb355cb
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-04-18 21:28:08 +0200

    Adds comments in the app

--- Third project/app/app.py
+++ Third project/app/app.py
@@ -19,6 +19,10 @@ class App(object):
 
     def __init__(self):
         # TODO: Parse the configuration file and start the program.
+
+        # We instantiate the main variables of our program and the models with
+        # the path of our DATABASE (that way we just have it in one place).
+
         self.nfc_sensor = NFCSensor()
         self.th_sensor = THSensor()
         self.notify = Notify()
@@ -30,14 +34,18 @@ class App(object):
 
     # Private methods
 
+    # Prepare threads is called to not cause a conflict between the main loop
+    # and the server that will be running in the port 5000.
     def _prepare_threads(self):
         self.app_thread = threading.Thread(target=self._run_web_app)
-        self.app_thread.daemon = True
+        self.app_thread.daemon = True # To close the app when we CTRL+C.
         self.app_thread.start()
 
     def _run_web_app(self):
         app.run()
 
+    # We are going to basically save the information to the database and
+    # perform all the changes that we've been asked we should do.
     def _perform_changes(self):
         initial_temperature = self._readings[0]
         final_temperature = self.th_sensor.get_data()
@@ -59,6 +67,12 @@ class App(object):
         try:
             while True:
                 data = self.nfc_sensor.get_data()
+
+                # It enters in the first if statement when a user reaches the
+                # NFC, that is no readings yet, now we are going to save the
+                # temperature and then save the initial data of the user, then
+                # wait for the user to leave, which will be when the data is
+                # None.
                 if data is not None and len(self._readings) == 0:
                     self._initial_data = data
                     self._readings.append(self.th_sensor.get_data())
