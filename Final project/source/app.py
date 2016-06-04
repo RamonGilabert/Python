6e91d2c19270b8328bacb2bb9633f1a8672ff279
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-06-04 19:46:32 +0200

    Adds the basic structure for the source

--- Final project/source/app.py
+++ Final project/source/app.py
@@ -8,14 +8,52 @@ from flask import Flask, request, session, g, redirect, url_for, \
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+# Index
+
 @app.route('/')
 def main_view():
     return render_template('index.html')
 
-@app.route('/temperatures')
+# Login
+
+@app.route('/login')
+def main_view():
+    return render_template('index.html')
+
+# Users
+
+@app.route('/users')
 def temperatures_view():
     temperatures = connection.get_temperatures()
     return render_template('temperatures.html', temperatures=temperatures)
+
+@app.route('/users/<int:id>')
+def temperatures_view(id):
+    temperatures = connection.get_temperatures()
+    return render_template('temperatures.html', temperatures=temperatures)
+
+@app.route('/new_user')
+def temperatures_view():
+    temperatures = connection.get_temperatures()
+    return render_template('temperatures.html', temperatures=temperatures)
+
+# Sensors
+
+@app.route('/sensors')
+def temperatures_view():
+    temperatures = connection.get_temperatures()
+    return render_template('temperatures.html', temperatures=temperatures)
+
+@app.route('/sensors/<int:id>')
+def temperatures_view(id):
+    temperatures = connection.get_temperatures()
+    return render_template('temperatures.html', temperatures=temperatures)
+
+@app.route('/new_sensor')
+def temperatures_view():
+    temperatures = connection.get_temperatures()
+    return render_template('temperatures.html', temperatures=temperatures)
+
 
 if __name__ == "__main__":
     connection.init_database()
