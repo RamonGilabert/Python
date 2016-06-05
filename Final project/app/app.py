0ca45cc6913cfa8b4d1cd56723a1ae71d9ef1406
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-06-05 22:06:26 +0200

    Starting to configure the main app

--- Final project/app/app.py
+++ Final project/app/app.py
@@ -11,9 +11,8 @@ sys.path.append('./../')
 from sensors.nfcsensor import NFCSensor
 from sensors.thsensor import THSensor
 from communication.notify import Notify
-from source.database.user import User
-from source.database.temperature import Temperature
-from source.app import app
+from web_app.app import app
+from web_service.app import web_service_app
 
 class App(object):
 
@@ -30,8 +29,8 @@ class App(object):
         self.nfc_sensor = NFCSensor()
         self.th_sensor = THSensor()
         self.notify = Notify(self._channels)
-        self.user_model = User(self._database)
-        self.temperature_model = Temperature(self._database)
+        # self.user_model = User(self._database)
+        # self.temperature_model = Temperature(self._database)
 
         self._prepare_threads()
         self._main_loop()
@@ -45,8 +44,15 @@ class App(object):
         self.app_thread.daemon = True # To close the app when we CTRL+C.
         self.app_thread.start()
 
+        self.web_service_thread = threading.Thread(target=self._run_web_service)
+        self.web_service_thread.daemon = True # To close the app when we CTRL+C.
+        self.web_service_thread.start()
+
     def _run_web_app(self):
         app.run()
+
+    def _run_web_service(self):
+        web_service_app.run()
 
     # We are going to basically save the information to the database and
     # perform all the changes that we've been asked we should do.
@@ -56,10 +62,10 @@ class App(object):
         difference = final_temperature - initial_temperature
         date = time.strftime('%d.%m.%Y at %H:%M')
 
-        self.user_model.add(self._initial_data['name'], \
-            self._initial_data['nfc'])
-        self.temperature_model.add(initial_temperature, date, \
-            difference, self._initial_data['nfc'])
+        # self.user_model.add(self._initial_data['name'], \
+        #     self._initial_data['nfc'])
+        # self.temperature_model.add(initial_temperature, date, \
+        #     difference, self._initial_data['nfc'])
         self.notify.broadcast(initial_temperature, difference)
 
     def _main_loop(self):
